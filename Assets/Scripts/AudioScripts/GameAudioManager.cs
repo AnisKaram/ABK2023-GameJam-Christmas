3e32967963dd4312f73ab361e0260cb787e83386
Anis karam <anisTkaram@hotmail.com>
2023-12-16 22:50:27 +0200

    Normalized all the music audios and sound effects
    - Normalized all the music audios.
    - Normalized all the sound effects.
    - Removed the old ones.

--- Assets/Scripts/AudioScripts/GameAudioManager.cs
+++ Assets/Scripts/AudioScripts/GameAudioManager.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameAudioManager : MonoBehaviour
@@ -11,6 +9,7 @@ public class GameAudioManager : MonoBehaviour
     public AudioClip walkingSFX;
     public AudioClip sprintingSFX;
     [SerializeField] float inGameVolume;
+    [SerializeField] float mainMenuVolume;
 
     private void Awake()
     {
@@ -19,17 +18,23 @@ public class GameAudioManager : MonoBehaviour
 
     public void PlayMusic(string name) {
         Sounds mySound = Array.Find(musicSounds, x => x.clipName == name);
-        if(mySound != null)
-        { 
+        if (mySound != null)
+        {
             musicSource.clip = mySound.clip;
             musicSource.loop = true;
-            if (mySound.clipName == "In Game Music") { musicSource.volume = inGameVolume; }
-            else { musicSource.volume = 0.5f; }
+            if (mySound.clipName == "In Game Music")
+            {
+                musicSource.volume = inGameVolume;
+            }
+            else
+            {
+                musicSource.volume = mainMenuVolume;
+            }
             musicSource.Play();
         }
         else
         {
-            Debug.Log($"Sound \"{name}\" not found (Possible Misspelling?)");
+            //Debug.Log($"Sound \"{name}\" not found (Possible Misspelling?)");
         }
     }
 
@@ -41,10 +46,12 @@ public class GameAudioManager : MonoBehaviour
             sfxSource.clip = mySound.clip;
             sfxSource.Play();
         }
-        else {
-            Debug.Log($"Sound \"{name}\" not found (Possible Misspelling?)");
+        else
+        {
+            //Debug.Log($"Sound \"{name}\" not found (Possible Misspelling?)");
         }
     }
+
     public void PlaySFXFromPlayer(string name)
     {
         Sounds mySound = Array.Find(sfxSounds, x => x.clipName == name);
@@ -55,7 +62,7 @@ public class GameAudioManager : MonoBehaviour
         }
         else
         {
-            Debug.Log($"Sound \"{name}\" not found (Possible Misspelling?)");
+            //Debug.Log($"Sound \"{name}\" not found (Possible Misspelling?)");
         }
     }
 
@@ -69,7 +76,7 @@ public class GameAudioManager : MonoBehaviour
         }
         else
         {
-            Debug.Log($"Sound \"{name}\" not found (Possible Misspelling?)");
+            //Debug.Log($"Sound \"{name}\" not found (Possible Misspelling?)");
         }
     }
 
@@ -79,6 +86,7 @@ public class GameAudioManager : MonoBehaviour
             footStepsSource.Play();
         }
     }
+
     public void sprintSFX() {
         if (footStepsSource.clip == null || footStepsSource.clip == walkingSFX)
         {
@@ -86,7 +94,9 @@ public class GameAudioManager : MonoBehaviour
             footStepsSource.Play();
         }
     }
-    public void standStill() {
+
+    public void standStill()
+    {
         footStepsSource.clip = null;
     }
 }
